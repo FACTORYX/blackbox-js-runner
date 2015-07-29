3d18474ec77be47c55dd920947ccdc05ea666a23
Steve Lacy <me@slacy.me>
2015-07-29 06:05:26 -0700

    such requires

--- runCode.js
+++ runCode.js
@@ -6,5 +6,10 @@ var code = fs.readFileSync(path.resolve(__dirname, '../code.js'), 'utf8');
 (function runEval () {
   'use strict';
   var done = function(){};
+  var req = require;
+  require = function(name) {
+    return req('/app/' + name);
+  };
+
   eval(code);
 })();
